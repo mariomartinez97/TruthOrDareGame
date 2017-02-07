39a57d4454ba6dbef40363646b2f53bb8be5fa13
m4-ma <m4-ma@DESKTOP-I5U3HPN>
2017-02-07 10:43:18 -0500

    3 buttons and text

---
+++ T_or_D/T_or_D/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace T_or_D
+{
+    [Activity(Label = "T_or_D", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+

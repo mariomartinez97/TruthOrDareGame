8140fd6e1c7e110ea879cf150c097a4af12ba633
m4-ma <m4-ma@DESKTOP-I5U3HPN>
2017-02-09 23:15:57 -0500

    CustomizedList logic finished

--- T_or_D/T_or_D/AddScreen.cs
+++ T_or_D/T_or_D/AddScreen.cs
@@ -21,6 +21,42 @@ namespace T_or_D
             SetContentView(Resource.Layout.AddScreen);
             // Create your application here
 
+            Button saveButton = FindViewById<Button>(Resource.Id.saveButton);
+            Button cancelButton = FindViewById<Button>(Resource.Id.cancelButton);
+
+            RadioGroup radioButton = FindViewById<RadioGroup>(Resource.Id.radioGroup1);
+            RadioButton checkedButton = FindViewById<RadioButton>(radioButton.CheckedRadioButtonId);            
+
+
+            saveButton.Click += (sender, e) =>
+            {
+                EditText inputText = FindViewById<EditText>(Resource.Id.textInput);
+                string input = inputText.Text;
+
+                var saveString = Application.Context.GetSharedPreferences("MySavedData", FileCreationMode.Private);
+                var edit = saveString.Edit();
+
+                if (checkedButton.Id == Resource.Id.radioButtonTruth)
+                {
+                    edit.PutString("Truths", input);
+                    edit.Commit();
+                    Android.Widget.Toast.MakeText(this, "Added to the list", ToastLength.Short).Show();
+                    inputText.Text = "";
+                }
+                if (checkedButton.Id == Resource.Id.radioButtonCancel)
+                {
+                    edit.PutString("Dares", input);
+                    edit.Commit();
+                    Android.Widget.Toast.MakeText(this, "Added to the list", ToastLength.Short).Show();
+                    inputText.Text = "";
+                }
+            };
+
+            cancelButton.Click += (sender, e) =>
+            {                                
+                var intent = new Intent(this, typeof(customizedList));
+                StartActivity(intent);
+            };
         }
     }
 }
--- T_or_D/T_or_D/customizedList.cs
+++ T_or_D/T_or_D/customizedList.cs
@@ -13,23 +13,67 @@ using Android.Widget;
 namespace T_or_D
 {
     [Activity(Label = "customizedList")]
-    public class customizedList : Activity
+    public class customizedList : ListActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.customizedList);
-            // Create your application here
 
-            //create list
-            List<string> listS = new List<string>();
-            //retrive old list if exists
+            Button add = FindViewById<Button>(Resource.Id.addButton);
+            Button continueButton = FindViewById<Button>(Resource.Id.continueButton);
+            Button cancelButton = FindViewById<Button>(Resource.Id.cancelButton);
+
+
+            add.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(AddScreen));
+                StartActivity(intent);
+            };
             
-            //add the new item to the list
+            //create list
+            List<string> listTruths = new List<string>();
+            List<string> listDares = new List<string>();
+            //retrive old list if exists
+            var sharedPref = Application.Context.GetSharedPreferences("MySavedData", FileCreationMode.Private);
+            if (sharedPref.Contains("TruthsList"))
+            {
+                listTruths.AddRange(sharedPref.GetStringSet("TruthsList",null).ToArray());
+            }
+            if (sharedPref.Contains("DaresList"))
+            {
+                listTruths.AddRange(sharedPref.GetStringSet("DaresList", null).ToArray());
+            }
 
+            //add the new item to the list
+            if (sharedPref.Contains("Truths"))
+            {
+                string newInput = sharedPref.GetString("Truths", null);
+                listTruths.Add(newInput);
+            }
+            if (sharedPref.Contains("Dares"))
+            {
+                string newInput = sharedPref.GetString("Dares", null);
+                listTruths.Add(newInput);
+            }
             //display list
+            List<string> completeList = new List<string>();            
+            completeList = listTruths.Concat(listDares).ToList();
+            ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, completeList);
 
             //save list
+            var saveLists = Application.Context.GetSharedPreferences("MySavedData", FileCreationMode.Private);
+            var edit = saveLists.Edit();
+
+            edit.PutStringSet("TruthsList", listTruths);
+            edit.PutStringSet("DaresList", listTruths);
+            edit.Commit();
+
+            cancelButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            };
 
         }
     }
